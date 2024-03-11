1e055dd454f5eabca6e2f6387f3af2f039ef4087
HugoSanche <hbmclaren@gmail.com>
2024-03-10 19:52:18 -0600

    example of serializacion

---
+++ Serializacion/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Persona persona=new Persona("Hugo",35);
+
+        //Serializacion
+        FileOutputStream fileOutputStream=new FileOutputStream("Persona.txt");
+        ObjectOutputStream objectOutStream=new ObjectOutputStream(fileOutputStream);
+        objectOutStream.writeObject(persona);
+        objectOutStream.close();
+
+        //Deserealizacion
+        FileInputStream fileInputStream=new FileInputStream("Persona.txt");
+        ObjectInputStream objectInputStream=new ObjectInputStream(fileInputStream);
+        Persona personaEnFichero=(Persona) objectInputStream.readObject();
+
+        System.out.println("Persona deserializada: "+personaEnFichero);
+    }
+}
